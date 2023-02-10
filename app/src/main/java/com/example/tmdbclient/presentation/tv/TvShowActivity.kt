cdd88cdb479985ebd300e7e8a41270eb3b5ab591
crmollinedo <cristianm184@hotmail.com>
2023-02-09 23:08:07 -0400

    Create Presentation Layer

---
+++ app/src/main/java/com/example/tmdbclient/presentation/tv/TvShowActivity.kt
@@ -0,0 +1,17 @@
+package com.example.tmdbclient.presentation.tv
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.tmdbclient.R
+import com.example.tmdbclient.databinding.ActivityTvShowBinding
+
+class TvShowActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityTvShowBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_tv_show)
+    }
+}
